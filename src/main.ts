b38f672cc0e006960956c3fd85ea652732b39543
gusxo999 <83279668+gusxo999@users.noreply.github.com>
2025-04-18 13:08:02 +0900

    프로젝트 시작 (#2)
    * 임시
    
    * 시작전 임시완성된 Prisma commit
    
    * 테스트
    
    * 프로젝트 시작전 확정된 Prisma 모델
    
    * .gitignore 추가
    
    ---------
    
    Co-authored-by: jbyoum <jbyoum3@gmail.com>

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import passport from './middlewares/passport/passport';
+import { PORT, UPLOAD_FOLDER, STATIC_PATH } from './config/constants';
+import exampleRouter from './routers/exampleRouter';
+import { defaultNotFoundHandler, globalErrorHandler } from './controllers/errorController';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(passport.initialize());
+
+app.use(STATIC_PATH, express.static(path.resolve(process.cwd(), UPLOAD_FOLDER)));
+
+app.use('/users', exampleRouter);
+
+app.use(defaultNotFoundHandler);
+app.use(globalErrorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
