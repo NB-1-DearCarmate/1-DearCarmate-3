9328ac87f554405bdb9f37b9eeb99c2d2013273d
gusxo999 <83279668+gusxo999@users.noreply.github.com>
2025-04-23 11:02:20 +0900

    feat/ 회사목록조회API 구현기능 squash #11 (#19)

--- src/controllers/company.ts
+++ src/controllers/company.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express';
-import { createCompany } from '../services/company';
+import { createCompany, getCompanies } from '../services/company';
 
 export const postCompany: RequestHandler = async (req, res, next) => {
   const { companyName, companyCode } = req.body;
@@ -7,4 +7,15 @@ export const postCompany: RequestHandler = async (req, res, next) => {
   const company = await createCompany({ companyName, companyCode });
 
   res.status(201).json(company);
+};
+
+export const getCompanyList: RequestHandler = async (req, res, next) => {
+  const page = parseInt(req.query.page as string, 10) || 1;
+  const pageSize = parseInt(req.query.pageSize as string, 10) || 10;
+  const searchBy = (req.query.searchBy as string) || '';
+  const keyword = (req.query.keyword as string) || '';
+
+  const companies = await getCompanies({ page, pageSize, searchBy, keyword });
+
+  res.status(200).json(companies);
 };
--- src/routers/company.ts
+++ src/routers/company.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { withAsync } from '../lib/withAsync';
-import { postCompany } from '../controllers/company';
+import { postCompany, getCompanyList } from '../controllers/company';
 
 const companyRouter = express.Router();
 
+companyRouter.get('/', withAsync(getCompanyList));
 companyRouter.post('/', withAsync(postCompany));
 
 export default companyRouter;
