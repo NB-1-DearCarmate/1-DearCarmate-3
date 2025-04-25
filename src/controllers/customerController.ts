95636ea712ef403c045b60b846749705b64c6100
HyeonTae Jang <gusxo999@gmail.com>
2025-04-25 14:32:13 +0900

    refactor/ 고객등록API 리팩토링 #33

--- src/controllers/customerController.ts
+++ src/controllers/customerController.ts
@@ -5,47 +5,9 @@ import UnauthError from '../lib/errors/UnauthError';
 import customerService from '../services/customerService';
 import { CreateCustomerBodyStruct } from '../structs/customerStructs';
 import { create } from 'superstruct';
-import { AGE_GROUP, REGION } from '@prisma/client';
 import userService from '../services/userService';
-
-const ageGroupMap: Record<string, AGE_GROUP> = {
-  '10대': AGE_GROUP.AGE_10,
-  '20대': AGE_GROUP.AGE_20,
-  '30대': AGE_GROUP.AGE_30,
-  '40대': AGE_GROUP.AGE_40,
-  '50대': AGE_GROUP.AGE_50,
-  '60대': AGE_GROUP.AGE_60,
-  '70대': AGE_GROUP.AGE_70,
-  '80대': AGE_GROUP.AGE_80,
-};
-
-const regionMap: Record<string, REGION> = {
-  서울: REGION.SEOUL,
-  경기: REGION.GYEONGGI,
-  인천: REGION.INCHEON,
-  강원: REGION.GANGWON,
-  충북: REGION.CHOUNGBUK,
-  충남: REGION.CHOUNGNAM,
-  세종: REGION.SEJONG,
-  대전: REGION.DAJEON,
-  경북: REGION.GYEONGBUK,
-  경남: REGION.GYEONGNAM,
-  광주: REGION.GWANGJU,
-  전북: REGION.JEONBUK,
-  전남: REGION.JEONNAM,
-  대구: REGION.DAEGU,
-  울산: REGION.ULSAN,
-  부산: REGION.BUSAN,
-  제주: REGION.JEJU,
-};
-
-const reverseAgeGroupMap: Record<AGE_GROUP, string> = Object.fromEntries(
-  Object.entries(ageGroupMap).map(([key, value]) => [value, key]),
-) as Record<AGE_GROUP, string>;
-
-const reverseRegionMap: Record<REGION, string> = Object.fromEntries(
-  Object.entries(regionMap).map(([key, value]) => [value, key]),
-) as Record<REGION, string>;
+import { PageParamsStruct } from '../structs/commonStructs';
+import { RequestCustomerDTO, ResponseCustomerDTO } from '../lib/dtos/customerDTO';
 
 export const getCustomer: RequestHandler = async (req, res) => {
   const reqUser = req.user as OmittedUser;
@@ -57,12 +19,12 @@ export const getCustomer: RequestHandler = async (req, res) => {
   //res.status(200).send(customer);
 };
 
-export const getCustomers: RequestHandler = async (req, res) => {
+export const getCustomerList: RequestHandler = async (req, res) => {
   const reqUser = req.user as OmittedUser;
   if (reqUser.role !== USER_ROLE.EMPLOYEE) {
     throw new UnauthError();
   }
-  //const page = create(req.query, PageParamsStruct);
+  const page = create(req.query, PageParamsStruct);
   //const customers = await customerService.getCustomers(page);
   // res.status(200).send(customers);
 };
@@ -74,17 +36,9 @@ export const postCustomer: RequestHandler = async (req, res) => {
   }
   const rawData = create(req.body, CreateCustomerBodyStruct);
   const userCompanyId = await userService.getCompanyIdById(reqUser.id);
-  const transformedData = {
-    ...rawData,
-    ageGroup: ageGroupMap[rawData.ageGroup],
-    region: regionMap[rawData.region],
-  };
+  const transformedData = new RequestCustomerDTO(rawData);
   const customer = await customerService.createCustomer(userCompanyId, transformedData);
-  const reverseTransformedData = {
-    ...customer,
-    ageGroup: reverseAgeGroupMap[customer.ageGroup as AGE_GROUP],
-    region: reverseRegionMap[customer.region as REGION],
-  };
+  const reverseTransformedData = new ResponseCustomerDTO(customer);
   res.status(201).send(reverseTransformedData);
 };
 
