671f0e85a72a39a013e26245a855e85f04d339ab
jbyoum <jbyoum3@gmail.com>
2025-04-30 18:24:11 +0900

    #42 라이브러리 제거

--- src/lib/fileUploader.ts
+++ src/lib/fileUploader.ts
@@ -1,15 +1,11 @@
-import { Request, Response } from 'express';
 import multer from 'multer';
 import path from 'path';
-import { fileTypeFromFile } from 'file-type';
-import fs from 'fs';
-import EmptyUploadError from '../lib/errors/EmptyUploadError';
-import FileExtError from '../lib/errors/FileExtError';
 
 interface UploadHandlerOptions {
   uploadFolder?: string;
   fileSizeLimit?: number;
   memoryFlag?: boolean;
+  allowedExt?: string[];
 }
 
 const dirname = path.resolve();
@@ -19,6 +15,7 @@ export function uploadHandler(options: UploadHandlerOptions) {
     uploadFolder = 'public/temp',
     fileSizeLimit = 100 * 1024 * 1024,
     memoryFlag = false,
+    allowedExt = [],
   } = options;
 
   const storage = multer.diskStorage({
@@ -31,13 +28,34 @@ export function uploadHandler(options: UploadHandlerOptions) {
       const baseName = path.basename(file.originalname, ext);
       const timestamp = Date.now();
       const uniqueFileName = `${baseName}-${timestamp}${ext}`;
+
       cb(null, uniqueFileName);
     },
   });
+  const fileFilter = (
+    req: Express.Request,
+    file: Express.Multer.File,
+    cb: multer.FileFilterCallback,
+  ) => {
+    if (allowedExt.length === 0) {
+      cb(null, true);
+      return;
+    }
+    const allowedTypes = new RegExp(`^(${allowedExt.join('|')})$`, 'i');
+    const extname = path.extname(file.originalname).toLowerCase();
+    const mimeType = allowedTypes.test(file.mimetype);
+
+    if (mimeType && allowedTypes.test(extname)) {
+      cb(null, true);
+      return;
+    }
+    cb(null, false);
+  };
 
   const upload_memory = multer({
     storage: multer.memoryStorage(),
     limits: { fieldNameSize: 100, fileSize: fileSizeLimit },
+    fileFilter: fileFilter,
   });
   if (memoryFlag) {
     return upload_memory;
@@ -46,24 +64,7 @@ export function uploadHandler(options: UploadHandlerOptions) {
   const upload_storage = multer({
     storage,
     limits: { fieldNameSize: 100, fileSize: fileSizeLimit },
+    fileFilter: fileFilter,
   });
   return upload_storage;
-}
-
-function hasFile(req: Request): req is Request & { file: Express.Multer.File } {
-  return !!req.file;
-}
-
-export async function mimeTypeVerifier(req: Request, uploadFolder: string, allowedExt: string[]) {
-  if (!hasFile(req)) throw new EmptyUploadError();
-  const filePath = path.join(dirname, uploadFolder, req.file.filename);
-  const mimeType = await fileTypeFromFile(filePath);
-  const ext = mimeType?.ext ?? null;
-  if (!ext || !allowedExt.includes(ext)) {
-    fs.unlink(filePath, (err) => {
-      if (err) console.error('Failed to delete file:', err);
-    });
-    throw new FileExtError();
-  }
-  return req.file;
 }
