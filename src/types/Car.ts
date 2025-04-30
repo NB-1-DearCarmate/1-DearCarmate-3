dbf0a03b76e3fe20fb706be55cb014fbf89d37e1
funfun <alpha5179@naver.com>
2025-04-30 16:21:43 +0900

    feat: Car type add

---
+++ src/types/Car.ts
@@ -0,0 +1,15 @@
+interface Car {
+  id: number;
+  companyId: number;
+  carNumber: string;
+  carModelId: number;
+  manufacturingYear: number;
+  mileage: number;
+  price: number;
+  accidentCount: number;
+  explanation: string;
+  accidentDetails: string;
+  status: string;
+}
+
+export default Car;
